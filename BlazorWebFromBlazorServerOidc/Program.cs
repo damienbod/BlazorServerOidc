14df13fdf51c66284cde704510f63f38c37cd3b8
damienbod <damien_bod@hotmail.com>
2024-01-06 09:18:46 +0100

    build fix

--- BlazorWebFromBlazorServerOidc/Program.cs
+++ BlazorWebFromBlazorServerOidc/Program.cs
@@ -1,5 +1,4 @@
 using BlazorWebFromBlazorServerOidc.Data;
-using BlazorWebFromBlazorWebFromBlazorServerOidc;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
