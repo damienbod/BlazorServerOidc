dc3c8fa6d2565ebd0c923747b3b678d6dd125ee2
damienbod <damien_bod@hotmail.com>
2024-12-31 09:08:52 +0100

    OpenIddict updates

--- IdentityProvider/Worker.cs
+++ IdentityProvider/Worker.cs
@@ -56,7 +56,7 @@ public class Worker : IHostedService
                     Permissions =
                     {
                         Permissions.Endpoints.Authorization,
-                        Permissions.Endpoints.Logout,
+                        Permissions.Endpoints.EndSession,
                         Permissions.Endpoints.Token,
                         Permissions.Endpoints.Revocation,
                         Permissions.GrantTypes.AuthorizationCode,
