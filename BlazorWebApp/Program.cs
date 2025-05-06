0144064023bfeb4a30926a5f850253d3d9b9f69b
damienbod <damien_bod@hotmail.com>
2025-05-06 19:58:37 +0200

    Update security

--- BlazorWebApp/Program.cs
+++ BlazorWebApp/Program.cs
@@ -1,36 +1,63 @@
 using BlazorWebApp.Components;
+using Microsoft.IdentityModel.Logging;
 
-namespace BlazorWebApp
+namespace BlazorWebApp;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddHttpContextAccessor();
+
+        var oidcConfig = builder.Configuration.GetSection("OpenIDConnectSettings");
+
+        builder.Services.AddAuthenticationCore();
+
+        builder.Services.AddAntiforgery(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.HeaderName = "X-XSRF-TOKEN";
+            options.Cookie.Name = "__Host-core-X-XSRF-TOKEN";
+            options.Cookie.SameSite = SameSiteMode.Strict;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        });
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+        builder.Services.AddSecurityHeaderPolicies()
+            .SetDefaultPolicy(SecurityHeadersDefinitions
+            .GetHeaderPolicyCollection(oidcConfig["Authority"], 
+                builder.Environment.IsDevelopment()));
 
-            var app = builder.Build();
+        builder.Services.AddCascadingAuthenticationState();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAntiforgery();
-
-            app.MapStaticAssets();
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
         }
+        else
+        {
+            IdentityModelEventSource.ShowPII = true; 
+            IdentityModelEventSource.LogCompleteSecurityArtifact = true;
+        }
+
+        app.UseSecurityHeaders();
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.MapLoginLogoutEndpoints();
+
+        app.Run();
     }
 }
---
+++ BlazorWebApp/SecurityHeadersDefinitions.cs
@@ -0,0 +1,42 @@
+﻿namespace BlazorWebApp;
+
+using Microsoft.AspNetCore.Builder;
+
+public static class SecurityHeadersDefinitions
+{
+    public static HeaderPolicyCollection GetHeaderPolicyCollection(string? idpHost, bool isDev)
+    {
+        ArgumentNullException.ThrowIfNull(idpHost);
+
+        var policy = new HeaderPolicyCollection()
+            .AddFrameOptionsDeny()
+            .AddContentTypeOptionsNoSniff()
+            .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+            .AddCrossOriginOpenerPolicy(builder => builder.SameOrigin())
+            .AddCrossOriginResourcePolicy(builder => builder.SameOrigin())
+            .AddCrossOriginEmbedderPolicy(builder => builder.RequireCorp()) // remove for dev if using hot reload
+            .AddContentSecurityPolicy(builder =>
+            {
+                builder.AddObjectSrc().None();
+                builder.AddBlockAllMixedContent();
+                builder.AddImgSrc().Self().From("data:");
+                builder.AddFormAction().Self().From(idpHost);
+                builder.AddFontSrc().Self();
+                builder.AddStyleSrc().Self().UnsafeInline();
+                builder.AddBaseUri().Self();
+                builder.AddFrameAncestors().None();
+
+                builder.AddScriptSrc().WithNonce().UnsafeInline();
+            })
+            .RemoveServerHeader()
+            .AddPermissionsPolicyWithDefaultSecureDirectives();
+
+        if (!isDev)
+        {
+            // maxage = one year in seconds
+            policy.AddStrictTransportSecurityMaxAgeIncludeSubDomains();
+        }
+
+        return policy;
+    }
+}
